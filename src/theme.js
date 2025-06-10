2162491721be0736b7c49ae8042ab09b5cda96b9
Kane BA <developer.innovaappstar@gmail.com>
2025-06-10 00:34:31 -0500

    poppins ADDED

--- src/theme.js
+++ src/theme.js
@@ -56,6 +56,11 @@ const theme = extendTheme({
     useSystemColorMode: false,
   },
   
+  fonts: {
+    heading: `'Poppins', -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"`,
+    body: `'Poppins', -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"`,
+  },
+  
   colors: createThemeColors('light'),
   
   semanticTokens: {
